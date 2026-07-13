0845f98254607d518efb48867d0d6aa633332721
Prashanth K <hprashanth92@gmail.com>
2026-07-13 08:48:03 +0530

    Update Provider.tf

--- Provider.tf
+++ Provider.tf
@@ -1,1 +1,10 @@
-
+provider "aws" {
+  region = var.aws_region
+  default_tags {
+    tags = {
+      Project     = var.project_name
+      Environment = var.environment
+      ManagedBy   = "Terraform"
+    }
+  }
+}
